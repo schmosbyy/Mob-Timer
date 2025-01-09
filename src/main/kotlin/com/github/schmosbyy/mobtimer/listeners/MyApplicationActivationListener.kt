f170797c021330ebd301f382b364295d7ff151e1
GitHub Action <action@github.com>
2025-01-09 00:03:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/schmosbyy/mobtimer/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/schmosbyy/mobtimer/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.schmosbyy.mobtimer.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
